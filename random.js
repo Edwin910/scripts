8c2f12b6530de629bba57fd01c389c71bb2f9546
Edwin910 <55273620+Edwin910@users.noreply.github.com>
2019-09-16 00:39:28 +0800

    Ii
    100

--- random.js
+++ random.js
@@ -13,14 +13,14 @@ while (true) {
   // the largest bet the profit limit will support or our balance, whichever is
   // smaller
   const maxBet = Math.floor(Math.min(this.balance, maxProfit / 0.01))
-  const betSize = Math.round((Math.random() * (maxBet - 100) + 100) / 100) * 100
+  const betSize = Math.round((Math.random() * (maxBet - 50) + 50) / 50) * 100
 
   const maxTarget = Math.min(
-    Math.floor((maxProfit / betSize + 1) * 100),
-    100000000
+    Math.floor((maxProfit / betSize + 1) * 50),
+    20000
   )
-  const target = Math.round(Math.random() * (maxTarget - 101) + 101) / 100
+  const target = Math.round(Math.random(5) * (maxTarget - 51) + 51) / 100
 
-  this.log(`Betting ${betSize/100} bits at ${target.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}x.`)
+  this.log(`Betting ${betSize/50} bits at ${target.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}x.`)
   await this.bet(betSize, target)
 }
